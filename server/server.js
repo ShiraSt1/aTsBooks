b872a5ad92ac0f84ff065b4bc94c7f7bc7474286
ShiraSt1 <shiristern24@gmail.com>
2025-06-13 17:44:24 +0300

    change client url

--- server/server.js
+++ server/server.js
@@ -35,10 +35,13 @@ app.use("/api/title", require("./routes/title.js"))
 app.use("/api/file", require("./routes/file.js"))
 app.use("/api/course", require("./routes/course.js"))
 
-app.use(express.static(path.join(__dirname, "client/build")));
 // כל בקשה אחרת – נשלח ל-React
+const pathToClientBuild = path.join(__dirname, "..", "client", "build");
+
+app.use(express.static(pathToClientBuild));
+
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "client/build", "index.html"));
+  res.sendFile(path.join(pathToClientBuild, "index.html"));
 });
 
 // app.get('/', (req, res) => { res.send("this is the home page") })
