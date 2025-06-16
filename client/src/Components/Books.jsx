e80fb7115b120a7ae3479a5b26fb4dd171d1f87d
ShiraSt1 <shiristern24@gmail.com>
2025-06-16 12:40:19 +0300

    add minheight to img style

--- client/src/Components/Books.jsx
+++ client/src/Components/Books.jsx
@@ -212,7 +212,7 @@ export default function BooksDataView() {
                         // src={`${process.env.REACT_APP_API_URL}${book.image}`}
                         alt={book.name}
                         // style={{ objectFit: 'cover' }}
-                        style={{ width: "100%", height: "100%", objectFit: "cover" }}
+                        style={{ width: "100%", height: "100%", objectFit: "cover", minHeight: '300px'  }}
                     />
                     <div className="text-2xl font-bold">{book.name}</div>
                     {book.grades && book.grades.length > 0 && (
