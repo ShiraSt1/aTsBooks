266e29cf4dfa36fd9bc95757b13e9c5395f4e0d5
ShiraSt1 <shiristern24@gmail.com>
2025-06-24 00:57:49 +0300

    serve Google verification as string

--- server/server.js
+++ server/server.js
@@ -58,13 +58,8 @@ app.get('/api/sitemap', (req, res) => {
 });
 
 app.get('/googleea171b815b7e0bf10.html', (req, res) => {
-  res.sendFile(path.resolve('public/googleea171b815b7e0bf10.html'));
+  res.send('google-site-verification: googleea171b815b7e0bf10.html');
 });
-
-app.get('/text.html', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'text.html'));
-});
-
 
 app.use("/api/user", require("./routes/user.js"))
 app.use("/api/book", require("./routes/book.js"))
