89fc40ebb6aaaa5a3264b1dfe63ea4bc4d75f7e9
ShiraSt1 <shiristern24@gmail.com>
2025-06-24 15:54:56 +0300

    fix import download i server.js

--- server/server.js
+++ server/server.js
@@ -45,7 +45,7 @@ app.use("/api/grade", require("./routes/grade.js"))
 app.use("/api/title", require("./routes/title.js"))
 app.use("/api/file", require("./routes/file.js"))
 app.use("/api/course", require("./routes/course.js"))
-app.use('/api', require("./routes/downloadRoutes.js"));
+app.use('/api', require("./routes/download.js"));
 
 const startServer = async () => {
   try {
